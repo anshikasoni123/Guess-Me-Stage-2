c7651f41cf424df1a0db5cac269268d8b30cec9b
anshikasoni123 <80338855+anshikasoni123@users.noreply.github.com>
2022-10-11 18:05:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, jsonify, request
+import random
+
+template = [
+    {
+        "inputs":"5",
+        "category":"Sports",
+        "word":"Chess"
+    },
+    {
+        "inputs":"6",
+        "category":"Europeon Country Name",
+        "word":"France"
+    }
+]
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/get-template")
+def get_template():
+    return jsonify({
+        "status":"success",
+        "word": random.choice(template)
+    })
+
+
+if __name__ == "__main__":
+    app.run()
